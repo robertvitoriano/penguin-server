8f27314c115f4c00c8901e17ca110e6a0c7640fe
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-03-25 23:59:20 -0300

    organize as mvc

---
+++ controllers/users.go
@@ -0,0 +1,56 @@
+package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/gorilla/mux"
+	"github.com/robertvitoriano/penguin-server/models"
+)
+
+var users = []models.User{
+	{ID: "1", Name: "Penguin 1"},
+	{ID: "2", Name: "Penguin 2"},
+}
+
+func GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+func GetUser(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	for _, user := range users {
+		if user.ID == params["id"] {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode(user)
+			return
+		}
+	}
+	http.Error(w, "User not found", http.StatusNotFound)
+}
+
+func CreateUser(responseWriter http.ResponseWriter, request *http.Request) {
+
+	min, max := 10, 255
+	var r = rand.Intn(max-min+1) + min
+	var g = rand.Intn(max-min+1) + min
+	var b = rand.Intn(max-min+1) + min
+	var a = rand.Intn(100)
+
+	newColor := fmt.Sprintf("rgba(%d, %d, %d, %.2f)", r, g, b, float64(a)/100)
+
+	var newUser models.User
+	json.NewDecoder(request.Body).Decode(&newUser)
+
+	newUser.Color = newColor
+	newUUID := uuid.New().String()
+	newUser.ID = newUUID
+	users = append(users, newUser)
+
+	responseWriter.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(responseWriter).Encode(newUser)
+}
--- main.go
+++ main.go
@@ -1,68 +1,19 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
-	"math/rand"
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/robertvitoriano/penguin-server/controllers"
 )
 
-type User struct {
-	ID    string `json:"id"`
-	Name  string `json:"name"`
-	Color string `json:"color"`
-}
-
-var users = []User{
-	{ID: "1", Name: "Penguin 1"},
-	{ID: "2", Name: "Penguin 2"},
-}
-
-func getUsers(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(users)
-}
-func getUser(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
-	for _, user := range users {
-		if user.ID == params["id"] {
-			w.Header().Set("Content-Type", "application/json")
-			json.NewEncoder(w).Encode(user)
-			return
-		}
-	}
-	http.Error(w, "User not found", http.StatusNotFound)
-}
-
-func createUser(responseWriter http.ResponseWriter, request *http.Request) {
-
-	min, max := 10, 255
-	var r = rand.Intn(max-min+1) + min
-	var g = rand.Intn(max-min+1) + min
-	var b = rand.Intn(max-min+1) + min
-	var a = rand.Intn(100)
-
-	newColor := fmt.Sprintf("rgba(%d, %d, %d, %.2f)", r, g, b, float64(a)/100)
-
-	var newUser User
-	json.NewDecoder(request.Body).Decode(&newUser)
-
-	newUser.Color = newColor
-	users = append(users, newUser)
-
-	responseWriter.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(responseWriter).Encode(newUser)
-}
-
 func main() {
-	r := mux.NewRouter()
-	r.HandleFunc("/users/{id}", getUser).Methods("GET")
-	r.HandleFunc("/users", createUser).Methods("POST")
-	r.HandleFunc("/users", getUsers).Methods("GET")
-
+	router := mux.NewRouter()
+	router.HandleFunc("/users/{id}", controllers.GetUser).Methods("GET")
+	router.HandleFunc("/users", controllers.CreateUser).Methods("POST")
+	router.HandleFunc("/users", controllers.GetUsers).Methods("GET")
 	fmt.Println("Server running on port 8080...")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	log.Fatal(http.ListenAndServe(":8080", router))
 }
