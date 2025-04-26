a7892483d504fd9e0cd65de175e02cc71b20d894
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-04-26 00:20:34 -0300

    remove options hangler

--- main.go
+++ main.go
@@ -18,21 +18,16 @@ func main() {
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
 	router := mux.NewRouter()
 	router.HandleFunc("/players/{id}", controllers.GetPlayer).Methods("GET")
 	router.HandleFunc("/players", func(w http.ResponseWriter, r *http.Request) {
 		controllers.CreatePlayer(w, r, ws)
 	}).Methods("POST")
 	router.HandleFunc("/players", controllers.GetPlayers).Methods("GET")
-
 	router.HandleFunc("/ws", ws.ServeWebsocket).Methods("GET")
-	router.Methods("OPTIONS").HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusOK)
-	})
-	fmt.Println("Server running on port 7777...")
 
 	c := cors.New(cors.Options{
-
 		AllowedOrigins: []string{
 			"http://localhost:8000",
 			"https://penguim-adventure.robertvitoriano.com",
@@ -40,9 +35,11 @@ func main() {
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Content-Type", "Content-Length", "Accept-Encoding", "X-CSRF-Token", "Authorization"},
 		AllowCredentials: true,
-		Debug:            true,
+		Debug:            false,
 	})
 
 	handler := c.Handler(router)
+
+	fmt.Println("Server running on port 7777...")
 	log.Fatal(http.ListenAndServe(":7777", handler))
 }
