794302f92def0e65dd5453c0ee6b110a6258690a
Robert Vitoriano <robertvitorianodev@gmail.com>
2025-05-12 22:13:16 -0300

    add docker and ecr deploy script

---
+++ docker/pull_and_run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+ECR_REGISTRY="<your-ecr-registry>"
+ECR_REPOSITORY="<your-ecr-repository>"
+IMAGE_TAG="latest"
+CONTAINER_NAME="penguin-server"
+REGION="<your-region>"
+
+aws ecr get-login-password --region $REGION | docker login --username AWS --password-stdin $ECR_REGISTRY
+
+# Get the latest image digest from ECR
+LATEST_DIGEST=$(aws ecr describe-images --repository-name $ECR_REPOSITORY --region $REGION --query "imageDetails[?imageTags[?contains(@, '$IMAGE_TAG')]].imageDigest" --output text)
+
+# Get the currently running container's image digest
+CURRENT_DIGEST=$(docker inspect --format='{{index .RepoDigests 0}}' $CONTAINER_NAME 2>/dev/null | awk -F'@' '{print $2}')
+
+# Compare digests
+if [ "$LATEST_DIGEST" == "$CURRENT_DIGEST" ]; then
+  echo "The container is already running the latest image. No action needed."
+  exit 0
+fi
+
+echo "New image detected. Updating the container..."
+
+docker pull $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
+
+docker stop $CONTAINER_NAME 
+docker rm $CONTAINER_NAME 
+
+docker run -d --name $CONTAINER_NAME -p 7777:7777 $ECR_REGISTRY/$ECR_REPOSITORY:$IMAGE_TAG
--- go.mod
+++ go.mod
@@ -2,8 +2,11 @@ module github.com/robertvitoriano/penguin-server
 
 go 1.24.0
 
+require gorm.io/driver/mysql v1.5.7
+
 require (
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
 	github.com/gorilla/mux v1.8.1 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
